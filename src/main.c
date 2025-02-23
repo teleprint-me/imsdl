f3c9cc4276d37da32222d1257f0146debb5ab829
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-22 23:00:37 -0500

    main(feat): Add initial sketch for handling mouse input

--- src/main.c
+++ src/main.c
@@ -5,15 +5,18 @@
 #include <stdio.h>
 
 // Don't over complicate this, keep this simple for now
-/// @note It's just the mouse state. Probably needs a better name.
-struct IMState { // maybe IMSDL_Mouse_State
-    int mouse_x;
-    int mouse_y;
-    int mouse_down;
-
+typedef struct IMSDL_Mouse_State {
+    int x;
+    int y;
+    int left_up;
+    int left_down;
+    int right_up;
+    int right_down;
+    int wheel_up;
+    int wheel_down;
     int hot;
     int active;
-};
+} IMSDL_Mouse_State;
 
 int main(void) {
     IMSDL_Viewport* viewport = imsdl_create_viewport("IMSDL", 800, 600, 0);
@@ -30,9 +33,49 @@ int main(void) {
     GLuint shader_program
         = imsdl_create_shader_program("shaders/vertex.glsl", "shaders/fragment.glsl");
 
+    IMSDL_Mouse_State mouse = {0};
+    mouse.x = mouse.y = 0;
     int running = 1;
     while (running) {
-        imsdl_handle_events(&running);
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = 0;
+                    break;
+                case SDL_MOUSEMOTION:
+                    mouse.x = event.motion.x;
+                    mouse.y = event.motion.y;
+                    LOG_INFO("Mouse: x=%d, y=%d", mouse.x, mouse.y);
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        mouse.left_down = 1;
+                        LOG_INFO("Mouse left down.");
+                    }
+                    if (event.button.button == SDL_BUTTON_RIGHT) {
+                        mouse.right_down = 1;
+                        LOG_INFO("Mouse right down.");
+                    }
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        mouse.left_up = 1;
+                        LOG_INFO("Mouse left up.");
+                    }
+                    if (event.button.button == SDL_BUTTON_RIGHT) {
+                        mouse.right_up = 1;
+                        LOG_INFO("Mouse right up.");
+                    }
+                    break;
+                case SDL_MOUSEWHEEL:
+                    if (event.wheel.y > 0) {
+                        LOG_INFO("Scrolling up.");
+                    } else {
+                        LOG_INFO("Scrolling down.");
+                    }
+            }
+        }
         imsdl_render(viewport, shader_program);
     }
 
